38e5245978297d0aafd11581b5f0b2d0dfc4ddd2
Carlos Molina <carlos170373@gmail.com>
2020-01-15 23:13:02 +0000

    logrado el login fb

--- src/js/component/loginFB.js
+++ src/js/component/loginFB.js
@@ -11,11 +11,9 @@ export default class Myfblogin extends Component {
 	/*handleResponse = data => {
 		console.log(data);
 		this.setState({
-			isLoggedin: true,
-			first_name: data.profile.first_name,
-			email: data.profile.email
+			isLoggedin: true
 		});
-    };*/
+	};*/
 
 	handleError = error => {
 		this.setState({ error });
@@ -26,6 +24,55 @@ export default class Myfblogin extends Component {
 	};
 
 	render() {
+		return (
+			<Context.Consumer>
+				{({ store, actions }) => {
+					if (store.fbobject.isLoggedin) {
+						return (
+							<div
+								style={{
+									width: "400px",
+									margin: "auto",
+									background: "#f4f4f4",
+									padding: "20px"
+								}}>
+								<div>
+									<h2>Welcome: {store.fbobject.first_name}</h2>
+									Email: {store.fbobject.email}
+								</div>
+							</div>
+						);
+					} else {
+						return (
+							<FacebookProvider appId="1030606370611311">
+								<LoginButton
+									scope="email"
+									onCompleted={actions.handleResponse}
+									onError={this.handleError}>
+									<div className="row">
+										<div className="col">
+											<button
+												className="btn btn-lg btn-facebook btn-block text-uppercase"
+												type="submit">
+												<i className="fab fa-facebook-f mr-2" /> {store.fbobject.first_name}
+												Ingresar con Facebook
+											</button>
+										</div>
+									</div>
+
+									<br />
+									<Status>{({ loading, status }) => <div> {status}</div>}</Status>
+								</LoginButton>
+							</FacebookProvider>
+						);
+					}
+				}}
+			</Context.Consumer>
+		);
+	}
+}
+
+/*render() {
 		let fbcontent;
 		if (this.state.isLoggedin) {
 			fbcontent = (
@@ -80,8 +127,4 @@ export default class Myfblogin extends Component {
 		}
 		return <div>{fbcontent}</div>;
 	}
-}
-
-/*export default function Myfblogin()
-onClick={() => actions.capturefb(datafb)}
-<LoginButton scope="email" onCompleted={this.handleResponse} onError={this.handleError}></LoginButton>*/
+}*/
